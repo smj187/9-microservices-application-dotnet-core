0f4416161d7cc82aca93216289bded59580f787b
smj187 <simon.jenewein@casablanca.at>
2022-05-30 01:06:31 +0200

    add rabbit and masstransit

---
+++ Backend/BuildingBlocks/MassTransit/RabbitMqSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildingBlocks.MassTransit
+{
+    public class RabbitMqSettings
+    {
+        public const string RabbitMqUri = "rabbitmq://localhost";
+
+
+        public const string Username = "guest";
+        public const string Password = "guest";
+    }
+}
